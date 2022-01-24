ad24c58070e106c7dbf019d2508b01a0aa043529
cap0n3 <cap0n3@protonmail.com>
2022-01-24 01:06:33 +0100

    Added echo lines in error handling

--- myBackupper.sh
+++ myBackupper.sh
@@ -223,6 +223,7 @@ for FOLDER_PATH in ${FOLDER_PATHS[@]}
 do
 	writeLog "[INFO] - Starting backup of '$FOLDER_PATH' in '$DEST_PATH'" "info"
 	#Start backup
+	#rsync -arh --info=progress2 $FOLDER_PATH $DEST_PATH => with progress bar
 	bckup_cmd=$(rsync -arhv $FOLDER_PATH $DEST_PATH 2>&1)
 	exit_code=$?
 	#If it's not a success
@@ -236,58 +237,72 @@ do
 		case "$exit_code" in
 			1)
 				writeLog "[RSYNC ERROR] - Syntax or usage error. (exit code 1)" "err"
+				echo -e "[RSYNC ERROR] - Syntax or usage error. (exit code 1)\n"
 				exit 1
 			;;
 			2)
 				writeLog "[RSYNC ERROR] - Protocol incompatibility. (exit code 2)" "err"
+				echo -e "[RSYNC ERROR] - Protocol incompatibility. (exit code 2)"
 				exit 1
 			;;
 			3)
 				writeLog "[RSYNC ERROR] - Errors selecting input/output files, dirs. (exit code 3)" "err"
+				echo -e "[RSYNC ERROR] - Errors selecting input/output files, dirs. (exit code 3)"
 				exit 1
 			;;
 			4)
 				writeLog "[RSYNC ERROR] - Requested action not supported: an attempt was made to manipulate 64-bit files on a platform that cannot support them; or an option was specified that is supported by the client and not by the server. (exit code 4)" "err"
+				echo -e "[RSYNC ERROR] - Requested action not supported: an attempt was made to manipulate 64-bit files on a platform that cannot support them; or an option was specified that is supported by the client and not by the server. (exit code 4)"
 				exit 1
 			;;
 			5)
 				writeLog "[RSYNC ERROR] - Error starting client-server protocol. (exit code 5)" "err"
+				echo -e "[RSYNC ERROR] - Error starting client-server protocol. (exit code 5)"
 				exit 1
 			;;
 			6)
 				writeLog "[RSYNC ERROR] - Daemon unable to append to writeLog-file. (exit code 6)" "err"
+				echo -e "[RSYNC ERROR] - Daemon unable to append to writeLog-file. (exit code 6)"
 				#NO EXIT
 			;;
 			10)
 				writeLog "[RSYNC ERROR] - Error in socket I/O. (exit code 10)" "err"
+				echo -e "[RSYNC ERROR] - Error in socket I/O. (exit code 10)"
 				exit 1
 			;;
 			11)
 				writeLog "[RSYNC ERROR] - Error in file I/O. (exit code 11)" "err"
+				echo -e "[RSYNC ERROR] - Error in file I/O. (exit code 11)"
 				exit 1
 			;;
 			12)
 				writeLog "[RSYNC ERROR] - Error in rsync protocol data stream. (exit code 12)" "err"
+				echo -e "[RSYNC ERROR] - Error in rsync protocol data stream. (exit code 12)"
 				exit 1
 			;;
 			13)
 				writeLog "[RSYNC ERROR] - Errors with program diagnostics. (exit code 13)" "err"
+				echo -e "[RSYNC ERROR] - Errors with program diagnostics. (exit code 13)"
 				#NO EXIT
 			;;
 			14)
 				writeLog "[RSYNC ERROR] - Error in IPC code. (exit code 14)" "err"
+				echo -e "[RSYNC ERROR] - Error in IPC code. (exit code 14)"
 				exit 1
 			;;
 			20)
 				writeLog "[RSYNC ERROR] - Received SIGUSR1 or SIGINT. (exit code 20)" "err"
+				echo -e "[RSYNC ERROR] - Received SIGUSR1 or SIGINT. (exit code 20)"
 				#NO EXIT
 			;;
 			21)
 				writeLog "[RSYNC ERROR] - Some error returned by waitpid(). (exit code 21)" "err"
+				echo -n "[RSYNC ERROR] - Some error returned by waitpid(). (exit code 21)"
 				#NO EXIT
 			;;
 			22)
 				writeLog "[RSYNC ERROR] - Error allocating core memory buffers. (exit code 22)" "err"
+				echo -e "[RSYNC ERROR] - Error allocating core memory buffers. (exit code 22)"
 				exit 1
 			;;
 			23)
@@ -295,41 +310,51 @@ do
 				#Store corrupted file paths for listing
 				corr_file_path=$(echo "$errMsg" | awk -F '"|"' '{print $2}')
 				corr_files+=($corr_file_path)
+				echo -e "[RSYNC ERROR] - Partial transfer due to error. (exit code 23). See logs for more infos."
 				#NO EXIT
 			;;
 			24)
 				writeLog "[RSYNC ERROR] - Partial transfer due to vanished source files. (exit code 24)" "err"
+				echo -e "[RSYNC ERROR] - Partial transfer due to vanished source files. (exit code 24)"
 				#NO EXIT
 			;;
 			25)
 				writeLog "[RSYNC ERROR] - The --max-delete limit stopped deletions. (exit code 25)" "err"
+				echo -e "[RSYNC ERROR] - The --max-delete limit stopped deletions. (exit code 25)"
 				#NO EXIT
 			;;
 			30)
 				writeLog "[RSYNC ERROR] - Timeout in data send/receive. (exit code 30)" "err"
+				echo -e "[RSYNC ERROR] - Timeout in data send/receive. (exit code 30)"
 				exit 1
 			;;
 			35)
 				writeLog "[RSYNC ERROR] - Timeout waiting for daemon connection. (exit code 35)" "err"
+				echo -e "[RSYNC ERROR] - Timeout waiting for daemon connection. (exit code 35)"
 				exit 1
 			;;
 			127)
 				writeLog "[RSYNC ERROR] - Rsync is not installed on your system ! Please install it before running the script (exit code 127)" "err"
+				echo -e "[RSYNC ERROR] - Rsync is not installed on your system ! Please install it before running the script (exit code 127)"
 				exit 1
 			;;
 		esac
 
 		#Only partial success because some non 'fatal' errors were encoutered.
 		writeLog "[PARTIAL SUCCESS] - '$FOLDER_PATH' partially saved in '$DEST_PATH'. Some errors were encountered." "info"
-		echo -e "\n\n[PARTIAL SUCCESS] - '$FOLDER_PATH' partially saved in '$DEST_PATH'. Some errors were encountered, see log or report for more infos."
+		echo -e "[PARTIAL SUCCESS] - '$FOLDER_PATH' partially saved in '$DEST_PATH'. Some errors were encountered, see log or report for more infos."
 	
 	else
 		writeLog "[SUCCESS] - '$FOLDER_PATH' successfully saved in '$DEST_PATH'. No error was encountered !" "info"
-		echo -e "\n\n[SUCCESS] - '$FOLDER_PATH' successfully saved in '$DEST_PATH'. No error was encountered !"
+		echo -e "[SUCCESS] - '$FOLDER_PATH' successfully saved in '$DEST_PATH'. No error was encountered !"
 	fi
 	
 	#ECHO message for user
-	echo -e "\n======== BCKUP_OUTPUT_INFO =======\n==================================\nSRC =>$FOLDER_PATH\n\n$bckup_cmd\n=================================="
+	echo -e "\n ================= BCKUP_OUTPUT_INFO ================"
+	echo -e " SRC => $FOLDER_PATH"
+	cmd_output=$(echo $bckup_cmd | tr -d '\t')
+	echo -e "$cmd_output"
+	echo -e " ====================================================\n"
 done
 
 #\\=============================================================#
