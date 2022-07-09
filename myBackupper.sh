f78140a172b5d1a880d1994d261cc3ecc59abc6c
cap0n3 <cap0n3@protonmail.com>
2022-07-10 01:16:30 +0200

    Corrected blank info when starting script

--- myBackupper.sh
+++ myBackupper.sh
@@ -1,6 +1,7 @@
 #!/bin/bash  
 
 source myBackupper.config
+source utils/colors.sh
 
 #==================================================#
 #================== MY BACKUPPER ==================#
@@ -222,87 +223,86 @@ fi
 for FOLDER_PATH in ${FOLDER_PATHS[@]}
 do
 	writeLog "[INFO] - Starting backup of '$FOLDER_PATH' in '$DEST_PATH'" "info"
+	echo -e "${CYAN}[INFO]${RESET} Starting backup of '$FOLDER_PATH' in '$DEST_PATH'"
 	#Start backup
-	#rsync -arh --info=progress2 $FOLDER_PATH $DEST_PATH => with progress bar
-	bckup_cmd=$(rsync -arhv $FOLDER_PATH $DEST_PATH 2>&1)
+	rsync -arh $FOLDER_PATH $DEST_PATH --info=progress2
 	exit_code=$?
+	
 	#If it's not a success
 	if [ "$exit_code" -ne 0 ]
 	then
-		#Something went wrong - Grep error line
-		errMsg=$(echo "$bckup_cmd" | grep rsync:)
 		#Create empty array to store corrupted file links (for report)
 		corr_files=()
 		#Log exact rsync error code for further investigation
 		case "$exit_code" in
 			1)
 				writeLog "[RSYNC ERROR] - Syntax or usage error. (exit code 1)" "err"
-				echo -e "[RSYNC ERROR] - Syntax or usage error. (exit code 1)\n"
+				echo -e "${RED}[RSYNC ERROR]${RESET} Syntax or usage error. (exit code 1)\n"
 				exit 1
 			;;
 			2)
 				writeLog "[RSYNC ERROR] - Protocol incompatibility. (exit code 2)" "err"
-				echo -e "[RSYNC ERROR] - Protocol incompatibility. (exit code 2)"
+				echo -e "${RED}[RSYNC ERROR]${RESET} Protocol incompatibility. (exit code 2)"
 				exit 1
 			;;
 			3)
 				writeLog "[RSYNC ERROR] - Errors selecting input/output files, dirs. (exit code 3)" "err"
-				echo -e "[RSYNC ERROR] - Errors selecting input/output files, dirs. (exit code 3)"
+				echo -e "${RED}[RSYNC ERROR]${RESET} Errors selecting input/output files, dirs. (exit code 3)"
 				exit 1
 			;;
 			4)
 				writeLog "[RSYNC ERROR] - Requested action not supported: an attempt was made to manipulate 64-bit files on a platform that cannot support them; or an option was specified that is supported by the client and not by the server. (exit code 4)" "err"
-				echo -e "[RSYNC ERROR] - Requested action not supported: an attempt was made to manipulate 64-bit files on a platform that cannot support them; or an option was specified that is supported by the client and not by the server. (exit code 4)"
+				echo -e "${RED}[RSYNC ERROR]${RESET} Requested action not supported: an attempt was made to manipulate 64-bit files on a platform that cannot support them; or an option was specified that is supported by the client and not by the server. (exit code 4)"
 				exit 1
 			;;
 			5)
 				writeLog "[RSYNC ERROR] - Error starting client-server protocol. (exit code 5)" "err"
-				echo -e "[RSYNC ERROR] - Error starting client-server protocol. (exit code 5)"
+				echo -e "${RED}[RSYNC ERROR]${RESET} Error starting client-server protocol. (exit code 5)"
 				exit 1
 			;;
 			6)
 				writeLog "[RSYNC ERROR] - Daemon unable to append to writeLog-file. (exit code 6)" "err"
-				echo -e "[RSYNC ERROR] - Daemon unable to append to writeLog-file. (exit code 6)"
+				echo -e "${RED}[RSYNC ERROR]${RESET} Daemon unable to append to writeLog-file. (exit code 6)"
 				#NO EXIT
 			;;
 			10)
 				writeLog "[RSYNC ERROR] - Error in socket I/O. (exit code 10)" "err"
-				echo -e "[RSYNC ERROR] - Error in socket I/O. (exit code 10)"
+				echo -e "${RED}[RSYNC ERROR]${RESET} Error in socket I/O. (exit code 10)"
 				exit 1
 			;;
 			11)
 				writeLog "[RSYNC ERROR] - Error in file I/O. (exit code 11)" "err"
-				echo -e "[RSYNC ERROR] - Error in file I/O. (exit code 11)"
+				echo -e "${RED}[RSYNC ERROR]${RESET} Error in file I/O. (exit code 11)"
 				exit 1
 			;;
 			12)
 				writeLog "[RSYNC ERROR] - Error in rsync protocol data stream. (exit code 12)" "err"
-				echo -e "[RSYNC ERROR] - Error in rsync protocol data stream. (exit code 12)"
+				echo -e "${RED}[RSYNC ERROR]${RESET} Error in rsync protocol data stream. (exit code 12)"
 				exit 1
 			;;
 			13)
 				writeLog "[RSYNC ERROR] - Errors with program diagnostics. (exit code 13)" "err"
-				echo -e "[RSYNC ERROR] - Errors with program diagnostics. (exit code 13)"
+				echo -e "${RED}[RSYNC ERROR]${RESET} Errors with program diagnostics. (exit code 13)"
 				#NO EXIT
 			;;
 			14)
 				writeLog "[RSYNC ERROR] - Error in IPC code. (exit code 14)" "err"
-				echo -e "[RSYNC ERROR] - Error in IPC code. (exit code 14)"
+				echo -e "${RED}[RSYNC ERROR]${RESET} Error in IPC code. (exit code 14)"
 				exit 1
 			;;
 			20)
 				writeLog "[RSYNC ERROR] - Received SIGUSR1 or SIGINT. (exit code 20)" "err"
-				echo -e "[RSYNC ERROR] - Received SIGUSR1 or SIGINT. (exit code 20)"
+				echo -e "${RED}[RSYNC ERROR]${RESET} Received SIGUSR1 or SIGINT. (exit code 20)"
 				#NO EXIT
 			;;
 			21)
 				writeLog "[RSYNC ERROR] - Some error returned by waitpid(). (exit code 21)" "err"
-				echo -n "[RSYNC ERROR] - Some error returned by waitpid(). (exit code 21)"
+				echo -n "${RED}[RSYNC ERROR]${RESET} Some error returned by waitpid(). (exit code 21)"
 				#NO EXIT
 			;;
 			22)
 				writeLog "[RSYNC ERROR] - Error allocating core memory buffers. (exit code 22)" "err"
-				echo -e "[RSYNC ERROR] - Error allocating core memory buffers. (exit code 22)"
+				echo -e "${RED}[RSYNC ERROR]${RESET} Error allocating core memory buffers. (exit code 22)"
 				exit 1
 			;;
 			23)
@@ -310,51 +310,44 @@ do
 				#Store corrupted file paths for listing
 				corr_file_path=$(echo "$errMsg" | awk -F '"|"' '{print $2}')
 				corr_files+=($corr_file_path)
-				echo -e "[RSYNC ERROR] - Partial transfer due to error. (exit code 23). See logs for more infos."
+				echo -e "${RED}[RSYNC ERROR]${RESET} Partial transfer due to error. (exit code 23). See logs for more infos."
 				#NO EXIT
 			;;
 			24)
 				writeLog "[RSYNC ERROR] - Partial transfer due to vanished source files. (exit code 24)" "err"
-				echo -e "[RSYNC ERROR] - Partial transfer due to vanished source files. (exit code 24)"
+				echo -e "${RED}[RSYNC ERROR]${RESET} Partial transfer due to vanished source files. (exit code 24)"
 				#NO EXIT
 			;;
 			25)
 				writeLog "[RSYNC ERROR] - The --max-delete limit stopped deletions. (exit code 25)" "err"
-				echo -e "[RSYNC ERROR] - The --max-delete limit stopped deletions. (exit code 25)"
+				echo -e "${RED}[RSYNC ERROR]${RESET} The --max-delete limit stopped deletions. (exit code 25)"
 				#NO EXIT
 			;;
 			30)
 				writeLog "[RSYNC ERROR] - Timeout in data send/receive. (exit code 30)" "err"
-				echo -e "[RSYNC ERROR] - Timeout in data send/receive. (exit code 30)"
+				echo -e "${RED}[RSYNC ERROR]${RESET} Timeout in data send/receive. (exit code 30)"
 				exit 1
 			;;
 			35)
 				writeLog "[RSYNC ERROR] - Timeout waiting for daemon connection. (exit code 35)" "err"
-				echo -e "[RSYNC ERROR] - Timeout waiting for daemon connection. (exit code 35)"
+				echo -e "${RED}[RSYNC ERROR]${RESET} Timeout waiting for daemon connection. (exit code 35)"
 				exit 1
 			;;
 			127)
 				writeLog "[RSYNC ERROR] - Rsync is not installed on your system ! Please install it before running the script (exit code 127)" "err"
-				echo -e "[RSYNC ERROR] - Rsync is not installed on your system ! Please install it before running the script (exit code 127)"
+				echo -e "${RED}[RSYNC ERROR]${RESET} Rsync is not installed on your system ! Please install it before running the script (exit code 127)"
 				exit 1
 			;;
 		esac
 
 		#Only partial success because some non 'fatal' errors were encoutered.
-		writeLog "[PARTIAL SUCCESS] - '$FOLDER_PATH' partially saved in '$DEST_PATH'. Some errors were encountered." "info"
-		echo -e "[PARTIAL SUCCESS] - '$FOLDER_PATH' partially saved in '$DEST_PATH'. Some errors were encountered, see log or report for more infos."
+		writeLog "[PARTIAL BACKUP] - '$FOLDER_PATH' partially saved in '$DEST_PATH'. Some errors were encountered." "info"
+		echo -e "${YELLOW}[PARTIAL BACKUP]${RESET} - '$FOLDER_PATH' partially saved in '$DEST_PATH'. Some errors were encountered, see log or report for more infos."
 	
 	else
 		writeLog "[SUCCESS] - '$FOLDER_PATH' successfully saved in '$DEST_PATH'. No error was encountered !" "info"
-		echo -e "[SUCCESS] - '$FOLDER_PATH' successfully saved in '$DEST_PATH'. No error was encountered !"
+		echo -e "${GREEN}[SUCCESS]${RESET} '$FOLDER_PATH' successfully saved in '$DEST_PATH'. No error was encountered !"
 	fi
-	
-	#ECHO message for user
-	echo -e "\n ================= BCKUP_OUTPUT_INFO ================"
-	echo -e " SRC => $FOLDER_PATH"
-	cmd_output=$(echo $bckup_cmd | tr -d '\t')
-	echo -e "$cmd_output"
-	echo -e " ====================================================\n"
 done
 
 #\\=============================================================#
@@ -373,9 +366,6 @@ for src_folders in "${FOLDER_PATHS[@]}";do echo -e "- $src_folders" >> "$DIR/bac
 echo -e "\nDESTINATION FOLDER :" >> "$DIR/backup_report.txt"
 echo -e "------------------\n" >> "$DIR/backup_report.txt"
 echo -e "- $DEST_PATH" >> "$DIR/backup_report.txt"
-# echo -e "\nRSYNC OUTPUT :" >> "$DIR/backup_report.txt"
-# echo -e "------------\n" >> "$DIR/backup_report.txt"
-# echo -e "$bckup_cmd" >> "$DIR/backup_report.txt"
 echo -e "\nERRORS :" >> "$DIR/backup_report.txt"
 echo -e "------\n" >> "$DIR/backup_report.txt"
 echo -e "[!] Total file errors : ${#corr_files[@]}" >> "$DIR/backup_report.txt"
---
+++ utils/colors.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Ansi color code variables
+RED="\e[0;91m"
+BLUE="\e[0;94m"
+GREEN="\e[0;92m"
+WHITE="\e[0;97m"
+BLACK="\e[0;30m"
+CYAN="\e[0;36m"
+PURPLE="\e[0;35m"
+YELLOW="\e[0;33m"
+EXPAND_BG="\e[K"
+BLUE_BG="\e[0;104m${expand_bg}"
+RED_BG="\e[0;101m${expand_bg}"
+GREEN_BG="\e[0;102m${expand_bg}"
+BOLD="\e[1m"
+ULINE="\e[4m"
+RESET="\e[0m"
